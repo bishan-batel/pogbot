c0ab0646cefb6b8949f10da9a9a063907010049e
bishan <schrodingerscat12@gmail.com>
2020-12-04 14:29:30 -0600

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import config from "./config/config";
 import discord, { Client } from "discord.js";
 import token from "./config/token";
+
 const client = new Client();
 
 client.on("ready", () => {
