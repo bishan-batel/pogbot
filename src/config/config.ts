f424371b3d623963afa677e1eea5b880e40d401f
bishan <schrodingerscat12@gmail.com>
2020-12-04 15:14:21 -0600

    added bias support & added phrases

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,26 +1,34 @@
 const config = {
-	eightBall: {
-		trigger: [
-			"Hey Mekala, pog?",
-			"Hey Conar, pog?",
-			"conar, pog?",
-			"mekala, pog?",
-		].reduce((prev, curr) => prev + "|" + curr, "poggers?"),
-		responses: [
+  key: /(poggers\?)|(weirdchamp\?)/,
+  defaultBias: 0.5,
+  bias: [
+    { key: /shea/, bias: 0.3 },
+
+    // Saurya being gay
+    { key: /(?=saurya)(?=gay)(?!not)/, bias: 1 },
+    { key: /(?=saurya)(?=gay)(?=not)/, bias: -1 },
+
+    { key: /jojo/, bias: -0.1 }, // Jojo is not poggers
+    { key: /cry/, bias: 0.2 }, // Crying is poggers
+  ],
+  phrases: {
+    pog: [
 			"hmm very poggers",
 			"that was so poggers",
-			"ngl kinda weirdchamp. . .",
 			"poggie woggie",
 			"poggers.",
-			"not pogchamp",
-			"pepehands.",
-			"I am not laughing and I am NOT pogging.",
 			"funny pog.",
-			"sadchamp",
-			"nochamp",
 			"pogchamp",
-		],
-	},
+    ],
+    notPog: [
+      "ngl kinda weirdchamp. . .",
+      "I am not laughing and I am NOT pogging.",
+      "not pogchamp",
+      "pepehands.",
+      "sadchamp",
+      "nochamp",
+    ]
+  }
 };
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,27 @@ client.on("ready", () => {
 });
 
 client.on("message", (msg: discord.Message) => {
+  const { content } = msg;
+
 	// bot check
   if (msg.author.bot) return;
   
 	// 8ball
-	if (msg.content.match(config.eightBall.trigger))
-		msg.reply(
-			config.eightBall.responses[
-				Math.floor(Math.random() * config.eightBall.responses.length)
-			]
-		);
+  if (content.match(config.key)) {
+    // Filters only bias options that match with message content
+    const matches = config.bias.filter(({ key }) => content.match(key));
+    
+    // Uses matched biases to calculate total bias
+    const totalBias = matches.reduce((prev, { bias }) => prev + bias, config.defaultBias);
+    let phrases: string[] = ["failed to retrieve any phrase"];
+    
+    // Assigns phrases based on bias
+    if (Math.random() < totalBias) phrases = config.phrases.pog;
+    else phrases = config.phrases.notPog;
+    
+    // Replies with random phrase
+    const randomIndex = Math.floor(Math.random() * (phrases.length));
+    msg.reply(phrases[randomIndex]);
+  } 
 });
 client.login(token);
