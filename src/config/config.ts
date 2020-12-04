594f327be9a8e4ff7fba7ebbfecb271cb2ba6f0c
Kishan Patel <schrodingerscat12@gmail.com>
2020-12-04 14:17:29 -0600

    lol made it work

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,26 @@
-export default {
-  
-}
+const config = {
+	eightBall: {
+		trigger: [
+			"Hey Mekala, pog?",
+			"Hey Conar, pog?",
+			"conar, pog?",
+			"mekala, pog?",
+		].reduce((prev, curr) => prev + "|" + curr, "poggers?"),
+		responses: [
+			"hmm very poggers",
+			"that was so poggers",
+			"ngl kinda weirdchamp. . .",
+			"poggie woggie",
+			"poggers.",
+			"not pogchamp",
+			"pepehands.",
+			"I am not laughing and I am NOT pogging.",
+			"funny pog.",
+			"sadchamp",
+			"nochamp",
+			"pogchamp",
+		],
+	},
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,16 @@ client.on("ready", () => {
 	console.log(`Logged in as ${client.user?.username}`);
 });
 
+client.on("message", (msg: discord.Message) => {
+	// bot check
+  if (msg.author.bot) return;
+  
+	// 8ball
+	if (msg.content.match(config.eightBall.trigger))
+		msg.reply(
+			config.eightBall.responses[
+				Math.floor(Math.random() * config.eightBall.responses.length)
+			]
+		);
+});
 client.login(token);
